4352277519b91e89b2cf1668701bed0c0cc2ee07
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-24 16:27:24 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,2 @@
-  
-sudo /etc/init.d/nginx stop
-sudo /etc/init.d/gunicorn stop
-sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-# sudo /etc/init.d/nginx start
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
-sudo /etc/init.d/nginx start
-# sudo /etc/init.d/gunicorn start
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
