47d5c7a6d7945e127374d0fa2528ffdb0293a741
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-24 13:37:51 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,1 +1,4 @@
-bind='0.0.0.0:8080'
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
