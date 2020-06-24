56653c3b06dbb3d894984fc5200ee1ef6a78e809
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-24 16:26:50 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,4 +1,14 @@
+  
 def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+    # for i in environ['QUERY_STRING']:
+    if environ['QUERY_STRING']:
+        s = str(environ['QUERY_STRING']).split('&')
+        data = bytes('\n'.join(s), encoding='utf8')
+    else:
+        data = b''
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ]
+    start_response(status, response_headers)
+    return iter([data])
