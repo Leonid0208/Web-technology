343a7fdfa6e65998f40b4ccafe8e43acedbbfe1d
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-24 16:16:24 +0300

    Update hello.py

--- etc/hello.py
+++ etc/hello.py
@@ -1,4 +1,2 @@
-def app(environ, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
-                  encoding="utf8")]
+pythonpath = '/home/box/web'
+bind = '0.0.0.0:8080'
