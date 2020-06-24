f14fe3d4a5f1bfeca88f4f5c27e0f92887bfc1d2
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-24 16:18:57 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,10 @@
-sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-sudo gunicorn - b 0.0.0.0:8080 hello:app
+  
+sudo /etc/init.d/nginx stop
+sudo /etc/init.d/gunicorn stop
+sudo rm /etc/nginx/sites-enabled/default
+sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+# sudo /etc/init.d/nginx start
+sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo /etc/init.d/nginx start
+# sudo /etc/init.d/gunicorn start
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello
