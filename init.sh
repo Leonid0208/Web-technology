a7fa79569fc6485339dc4160ca0d61717f04fb56
Leonid Emelyanenko <54509233+Leonid0208@users.noreply.github.com>
2020-06-30 11:09:42 +0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,10 +1,6 @@
-virtualenv -p python3 myvenv
-source myvenv/bin/activate
-sudo apt update
-sudo apt install python3.5
-sudo apt-get install -y python3.5-dev
-sudo rm /usr/bin/python3
-sudo ln -s /usr/bin/python3.5 /usr/bin/python3
-sudo pip3 install --upgrade pip
-sudo -H pip3 install --upgrade django==1.11.5
-sudo -H pip3 install --upgrade gunicorn
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
