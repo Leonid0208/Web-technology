a25a7b17005c4a33c07baf46bd156776b68faeda
Leonid0208 <themusik.batbka@gmail.com>
2020-07-04 11:23:46 +0300

    Add

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -3,7 +3,16 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField(max_length=255)
     text = models.TextField()
     added_at = models.DateTimeField(blank=True, auto_now_add=True)
@@ -13,14 +22,6 @@ class Question(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
 
 
 class Answer(models.Model):
