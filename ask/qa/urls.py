7b21090c8b982d3a02619cbf9e545d47f73acc94
Leonid0208 <themusik.batbka@gmail.com>
2020-07-05 15:06:58 +0300

    Add

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -4,11 +4,5 @@ from . import views
 
 
 urlpatterns = {
-    url(r'^$', views.test),
-    url(r'^login/$', views.test),
-    url(r'^signup/$', views.test),
-    url(r'^ask/$', views.test),
-    url(r'^popular/$', views.test),
-    url(r'^new/$', views.test),
-    url(r'^question/(?P<pk>\d+)/$', views.test),
+    url(r'^(?P<num>\d+)/$', views.question),
 }
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -6,9 +6,23 @@ from .models import Question
 
 def question(request):
     posts = Question.objects.all().order_by('-id')
-    limit = request.GET.get('limit', 10)
-    page = request.GET.get('page', 1)
-    paginator = Paginator(posts, limit)
+    page = request.GET.get('page')
+    paginator = Paginator(posts, 10)
+    paginator.baseurl = '/?page='
+    page = paginator.page(page)
+    return render(request, 'list.html',
+                  {'title': 'Latest',
+                   'paginator': paginator,
+                   'questions': page.object_list,
+                   'page': page,
+                   'user': request.user,
+                   'session': request.session, })
+
+
+def question_famous(request):
+    posts = Question.objects.all().order_by('-rating')
+    page = request.GET.get('page')
+    paginator = Paginator(posts, 10)
     paginator.baseurl = '/popular/?page='
     page = paginator.page(page)
     return render(request, 'list.html',
