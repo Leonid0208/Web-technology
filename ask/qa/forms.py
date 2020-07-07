639846ce49159e56cb0e1bc9e15cc2b84cfb0ee9
Leonid0208 <themusik.batbka@gmail.com>
2020-07-07 09:22:44 +0300

    Add

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -4,29 +4,38 @@ from .models import Question, Answer
 
 
 class AskForm(forms.Form):
-    title = forms.CharField()
+    title = forms.CharField(max_length=100)
     text = forms.CharField(widget=forms.Textarea)
 
     def clean(self):
         pass
 
     def save(self):
-        post = Question(**self.cleaned_data)
-        post.author_id = self._user.id
-        post.save()
-        return post
+        question = Question(**self.cleaned_data)
+        question.author_id = self._user.id
+        question.save()
+        return question
 
 
 class AnswerForm(forms.Form):
     text = forms.CharField(widget=forms.Textarea)
     question = forms.IntegerField(widget=forms.HiddenInput)
 
+    def clean_question(self):
+        question_id = self.cleaned_data['question']
+        try:
+            question = Question.objects.get(id=question_id)
+        except Question.DoesNotExist:
+            question = None
+        return question
+
     def clean(self):
         pass
 
     def save(self):
-        post = Answer(**self.cleaned_data)
-        post.author_id = self._user.id
-        post.save()
-        return post
+        answer = Answer(**self.cleaned_data)
+        answer.author_id = self._user.id
+        answer.save()
+        return answer
+
 
