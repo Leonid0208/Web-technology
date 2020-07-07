6f67168fb6ded0fdc8afb81bf575f2694081fd72
Leonid0208 <themusik.batbka@gmail.com>
2020-07-07 10:32:10 +0300

    Add

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -4,35 +4,26 @@ from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 
 
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
-
-
 class Question(models.Model):
-    objects = QuestionManager()
-    title = models.CharField(max_length=255)
-    text = models.TextField()
-    added_at = models.DateTimeField(blank=True, auto_now_add=True)
+    title = models.CharField(default="", max_length=1024)
+    text = models.TextField(default="")
+    added_at = models.DateField(null=True)
     rating = models.IntegerField(default=0)
-    author = models.ForeignKey('auth.User')
-    likes = models.ManyToManyField(User, related_name='likes_set')
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    likes = models.ManyToManyField(User, related_name="q_to_likes")
 
     def __str__(self):
         return self.title
 
     def get_url(self):
-        return 'question/{}/'.format(self.id)
+        return "/question/{}/".format(self.id)
 
 
 class Answer(models.Model):
     text = models.TextField(default="")
     added_at = models.DateField(null=True)
     question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
     def __str__(self):
         return self.text
